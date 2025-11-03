c5e0fa07c6a284143499818b29a7ab4b5135f77e
Vinamra Parashar <parasharvin101@gmail.com>
2025-11-03 21:31:25 +0530

    Update AuthContext.tsx

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -27,7 +27,7 @@ const DEMO_USERS = [
     password: 'password1', 
     name: 'Vinamra Parashar', 
     accountNumber: '23310972560',
-    balance: 25630
+    balance: 256374
   },
   { 
     username: 'user2', 
